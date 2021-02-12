b4aa2c57cdcb87a44b975cf11a3507542d5c4085
Markus Fruhmann <markus.fruhmann99@gmail.com>
2021-02-12 14:25:35 +0100

    Fixed video settings

--- resources/views/project/view.blade.php
+++ resources/views/project/view.blade.php
@@ -11,10 +11,11 @@
 <div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-column">
     @if(!empty($moviePaths))
         @foreach($moviePaths as $moviePath)
-            <video {{--width="320" height="240"--}} controls>
+            <video controls controlsList="nodownload" muted autoplay loop>
                 <source src="{{ url($moviePath) }}" type="video/mp4">
                 Your browser does not support the video tag.
             </video>
+            <br>
         @endforeach
     @else
         <div class="text-center py-5 w-100">
@@ -40,6 +41,7 @@
     @if(!empty($picturePaths))
         @foreach($picturePaths as $picturePath)
             <img src="{{ url($picturePath) }}" alt="Aktuelles Bild von {{ $project->name }}">
+            <br>
         @endforeach
     @else
         <div class="text-center py-5 w-100">
