76a59b8d99f099bcf8a85ae869f1b962e4231ddf
Markus Fruhmann <markus.fruhmann99@gmail.com>
2021-02-12 13:27:58 +0100

    Created detailed view for latest videos/pictures

---
+++ app/Orchid/Screens/Project/ProjectViewScreen.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Orchid\Screens\Project;
+
+use App\Models\Project;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Orchid\Screen\Action;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
+use Orchid\Support\Facades\Layout;
+
+class ProjectViewScreen extends Screen
+{
+    /**
+     * Display header name.
+     *
+     * @var string
+     */
+    public $name = '';
+
+    /**
+     * Display header description.
+     *
+     * @var string|null
+     */
+    public $description = 'Detailansicht';
+
+    /**
+     * Query data.
+     *
+     * @return array|RedirectResponse
+     */
+    public function query($id)
+    {
+        $project = Auth::user()->projects()->where('projects.id', '=', $id)->get()->first();
+
+        if (Auth::user()->hasAccess('manager') || Auth::user()->hasAccess('admin'))
+            $project = Project::query()->where('projects.id', '=', $id)->get()->first();
+
+        $this->name = $project->name;
+
+        $userSymlinks = Auth::user()->symlinks()
+            ->where('project_id', '=', $project->id)
+            ->where('is_movies', '=', false)->get();
+        $picturePaths = array();
+
+        foreach ($userSymlinks as $userSymlink) {
+            $symlink = $userSymlink->symlink;
+            $dirs = scandir(public_path('img/') . $symlink, SCANDIR_SORT_DESCENDING);
+            $latestDir = 'img/' . $symlink . '/' . $dirs[0];
+
+            if (str_contains($dirs[0], '.php'))
+                $latestDir = 'img/' . $symlink . '/' . $dirs[1];
+
+            $pictures = scandir(public_path($latestDir), SCANDIR_SORT_DESCENDING);
+            $latestPicture = $latestDir . '/' . $pictures[0];
+
+            if (str_contains($pictures[0], '.php'))
+                $latestPicture = 'img/' . $symlink . '/' . $pictures[1];
+
+            $picturePaths[] = $latestPicture;
+        }
+
+        $movieSymlinks = Auth::user()->symlinks()
+            ->where('project_id', '=', $project->id)
+            ->where('is_movies', '=', true)->get();
+        $moviePaths = array();
+
+        foreach ($movieSymlinks as $movieSymlink) {
+            $movies = scandir(public_path('img/') . $movieSymlink->symlink, SCANDIR_SORT_ASCENDING);
+
+            foreach ($movies as $movie) {
+                if (!strcmp($movie, '.') || !strcmp($movie, '..') || str_contains($movie, '.php'))
+                    continue;
+
+                $moviePaths[] = 'img/' . $movieSymlink->symlink . '/' . $movie;
+            }
+        }
+
+        return [
+            'project' => $project,
+            'picturePaths' => $picturePaths,
+            'moviePaths' => $moviePaths,
+        ];
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return Action[]
+     */
+    public function commandBar(): array
+    {
+        return [];
+    }
+
+    /**
+     * Views.
+     *
+     * @return Layout[]|string[]
+     */
+    public function layout(): array
+    {
+        return [
+            Layout::view('project.view')
+        ];
+    }
+}
---
+++ resources/views/project/view.blade.php
@@ -0,0 +1,51 @@
+<div class="col p-0 px-3">
+    <legend class="text-black">
+        @if (count($moviePaths) > 1)
+            {{ $project->name . ' - aktuelle Filme' }}
+            @else
+            {{ $project->name . ' - aktueller Film' }}
+            @endif
+    </legend>
+</div>
+
+<div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-column">
+    @if(!empty($moviePaths))
+        @foreach($moviePaths as $moviePath)
+            <video {{--width="320" height="240"--}} controls autoplay loop>
+                <source src="{{ $moviePath }}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        @endforeach
+    @else
+        <div class="text-center py-5 w-100">
+            <h3 class="font-weight-light">
+                Leider gibt es keinen aktuellen Film für dieses Projekt!
+            </h3>
+        </div>
+    @endif
+</div>
+
+
+<div class="col p-0 px-3">
+    <legend class="text-black">
+        @if (count($picturePaths) > 1)
+            {{ $project->name . ' - aktuelle Bilder' }}
+        @else
+            {{ $project->name . ' - aktuelles Bild' }}
+        @endif
+    </legend>
+</div>
+
+<div class="bg-white rounded shadow-sm p-4 py-4 d-flex flex-column">
+    @if(!empty($picturePaths))
+        @foreach($picturePaths as $picturePath)
+            <img src="{{ url($picturePath) }}" alt="Aktuelles Bild von {{ $project->name }}">
+        @endforeach
+    @else
+        <div class="text-center py-5 w-100">
+            <h3 class="font-weight-light">
+                Leider gibt es kein aktuelles Bild für dieses Projekt!
+            </h3>
+        </div>
+    @endif
+</div>
