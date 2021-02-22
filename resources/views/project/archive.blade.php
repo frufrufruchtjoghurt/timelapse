71dd07b3e064133c6c2c38e86f8567de882df284
Markus Fruhmann <markus@fruhmann.dev>
2021-02-22 13:42:24 +0100

    Camera enumeration per project starts with 1 in archive

--- resources/views/project/archive.blade.php
+++ resources/views/project/archive.blade.php
@@ -1,5 +1,5 @@
 @foreach($symlinks as $id => $symlink)
     <a href="{{ url($symlink) }}" class="btn btn-xs btn-success pull-left">
-        <x-orchid-icon path="camrecorder"/> &#8287; Kamera {{ $id }}
+        <x-orchid-icon path="camrecorder"/> &#8287; Kamera Nr. {{ $id + 1 }}
     </a>
 @endforeach
