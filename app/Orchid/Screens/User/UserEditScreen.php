63f61a49ea85b8e370d99e2bd7324a6a9ed7d438
Markus Fruhmann <markus@fruhmann.dev>
2021-02-22 13:31:34 +0100

    New json encode for standard permission at user creation

--- app/Orchid/Screens/User/UserEditScreen.php
+++ app/Orchid/Screens/User/UserEditScreen.php
@@ -149,7 +149,11 @@ class UserEditScreen extends Screen
             ->collapse()
             ->toArray();
 
-        array_push($permissions, ['platform.index' => true]);
+        $tmp_perm = json_decode($permissions, true);
+        $std_perm['platform.index'] = true;
+
+        array_push($tmp_perm, $std_perm);
+        $permissions = json_encode($tmp_perm);
 
         if (!$this->exists)
         {
