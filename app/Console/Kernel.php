d4423f148e191df395bf85ae0e17e09c83cbc0fc
Markus Fruhmann <markus@fruhmann.dev>
2021-02-23 13:54:09 +0100

    Prototype for symlink to latest pictures

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,14 @@
 
 namespace App\Console;
 
+use App\Models\Camera;
+use App\Models\Project;
 use App\Models\Symlink;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
@@ -39,6 +42,27 @@ class Kernel extends ConsoleKernel
                 $symlink->delete();
             }
         })->everyFiveMinutes();
+
+        $schedule->call(function () {
+            $projects = Project::all();
+
+            foreach ($projects as $project) {
+                $latest_content = Storage::disk('systems')->allFiles(sprintf('P%04d-%s/latest', $project->id, $project->name));
+
+                if (in_array(preg_match('/^pic[0-9][0-9][0-9]$/', $latest_content), $latest_content) && $project->is_dismantled)
+                    continue;
+
+                foreach (Storage::disk('systems')->allFiles(sprintf('P%04d-%s', $project->id, $project->name))
+                    as $dir) {
+                    if (!preg_match('/^cam[0-9][0-9][0-9]$/', $dir))
+                        continue;
+
+                    $cam_path = Storage::disk('systems')->url(sprintf('P%04d-%s/%s',
+                        $project->id, $project->name, $dir));
+                    $camera = Camera::query()->where('name', '=', $dir)->get()->first();
+                }
+            }
+        })->everyMinute();
     }
 
     /**
